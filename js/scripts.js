3b1d3ab5d3b70d18c7e8e3d9ae6f35c4e69c7575
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-08 10:29:37 -0800

    remove unused functions

--- js/scripts.js
+++ js/scripts.js
@@ -16,26 +16,6 @@ function Pizza(size, toppings){
 
 }
 
-// Pizza.prototype.addToList = function(){
-//   var li=document.createElement("li");
-//   var node = document.createTextNode(this.pizzaSize + this.pizzaToppings + "($)" + this.pizzaPrice.toFixed(2));
-//   li.appendChild(node);
-//   var ul = document.getElementById("pizzaList");
-//   ul.appendChild(li);
-// };
-
-// Pizza.prototype.submitList = function(x){
-//   var input = document.createElement("input");
-//   var node = document.createTextNode(this.pizzaSize + this.pizzaToppings + "($)" + this.pizzaPrice.toFixed(2));
-//   input.appendChild(node);
-//   input.name = "orderItem";
-//   input.value =this.pizzaSize + this.pizzaToppings + "($)" + this.pizzaPrice.toFixed(2);
-//   var ul = document.getElementById(x);
-//   ul.appendChild(input);
-// };
-
-
-
 Pizza.prototype.addToList = function(){
   var li=document.createElement("li");
   var node = document.createTextNode(this.pizzaSize + this.pizzaToppings + "($)" + this.pizzaPrice.toFixed(2));
@@ -57,14 +37,10 @@ var displayList = function (orderList, count){
   }
 };
 
-// var clearList = function (){
-//   for (var i = 0; i < count; i++)
-//   {
-//
-//     var ul = document.getElementById("displayOrder");
-//     $(this).remove();
-//   }
-// };
+var clearList = function (){
+  var x = document.getElementById("pizzaList");
+  x.remove(x.selectedIndex);
+};
 
 $(document).ready(function(){
   var total = 0;
@@ -74,52 +50,29 @@ $(document).ready(function(){
     $(this).remove();
     $("#pizzaDetailsDiv").show();
   });
-
     $("#addPizzaButton").click(function(){
-
     var size = "";
     var toppings = [];
     size = $("#pizzaDetailsDiv input[type='radio']:checked").val();
     $("#pizzaDetailsDiv input[type='checkbox']:checked").each(function(){
       toppings.push($(this).val());
     });
-
     var myPizza = new Pizza(size, toppings);
     total += myPizza.pizzaPrice;
-
-
     $("#customerOrder").show();
     $("#continueToCheckoutButton").show();
-
     myPizza.addToList();
     orderList[count] = myPizza;
     count++;
-
     $("#showTotal").text("$" + total.toFixed(2));
-
   });
-
   $("#continueToCheckoutButton").click(function(){
-
     $("html, body").animate({
       scrollTop: $("#form1").offset().top
     }, 2000);
     $("#showForm").hide();
     $("#form1").show();
-
       displayList(orderList, count);
-
-      // document.getElementById("displayOrder").innerHTML = Object.values(orderList[i]);
-
-
-
-  });
-  // $("clearFormButton").submit(function(event){
-  //   event.preventDefault();
-  //   clearList();
-  //   var total = 0;
-  //   var count = 0;
-  //   var orderList = [];
-  // });
+  }); // document.getElementById("displayOrder").innerHTML = Object.values(orderList[i]);
     $("#customerDetailsSubmitButton").submit();
 });
